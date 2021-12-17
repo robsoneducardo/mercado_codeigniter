701e6af8f76d109ffc4f7a27f05ad9ee0fbd3de5
robsoneducardo <robsoneducardo@gmail.com>
2021-12-17 05:24:09 -0300

    Categoria controller and model created

---
+++ app/Controllers/CategoriaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class CategoriaController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
